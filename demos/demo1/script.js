7624b4da7ab1384b6b13a72262715e6e3a9ac668
htcryme <56440776+htcryme@users.noreply.github.com>
2020-08-15 17:59:24 +0200

    demo1 created

---
+++ demos/demo1/script.js
@@ -0,0 +1,43 @@
+var can
+var c;
+
+var element;
+
+function adjustScreen() {
+    can = document.getElementById("canvas");
+    can.width = can.height = Math.min(window.innerWidth, window.innerHeight);
+    c = WebGlContext(can);
+}
+
+window.onresize = function () {
+    adjustScreen();
+}
+
+window.onload = function () {
+    adjustScreen();
+
+    c.clearColor(0.0, 0.0, 0.0, 0.0);
+
+    element = c.createElement();
+
+    element.attributes = [
+        c.createAttribute("vertPosition", 2),
+        c.createAttribute("vertColor", 3)
+    ];
+
+    element.vertices = [
+        0.0, 0.25, 1.0, 0.0, 0.0,
+        0.5, -0.25, 0.0, 1.0, 0.0,
+        -0.5, -0.25, 0.0, 0.0, 1.0
+    ];
+
+    element.indicies = [1, 2, 3];
+
+    var vertexShaderSrc = document.getElementById("vertexShader").innerHTML;
+    var fragmentShaderSrc = document.getElementById("fragmentShader").innerHTML;
+    c.addElement(element, vertexShaderSrc, fragmentShaderSrc);
+
+    c.clear();
+
+    c.renderFrame();
+}
