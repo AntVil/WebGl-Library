7331b699b503575963e797e8f7b0588c41fce289
htcryme <56440776+htcryme@users.noreply.github.com>
2020-08-16 20:33:17 +0200

    fixing demo 4

--- demos/demo4/script.js
+++ demos/demo4/script.js
@@ -9,9 +9,9 @@ function adjustScreen() {
     can = document.getElementById("canvas");
     can.width = Math.min(window.innerWidth, window.innerHeight);
     can.height = can.width;
-    if(c === undefined){
+    if (c === undefined) {
         c = new WebGlContext(can);
-    }else{
+    } else {
         c.adjustScreen();
     }
 }
@@ -23,7 +23,7 @@ window.onresize = function () {
 window.onload = function () {
     adjustScreen();
 
-    c.clearColor(0.0, 0.0, 0.0, 0.0);
+    c.clearColor(0, 0, 0, 1);
 
     element = c.createElement();
     element.attributes = [
@@ -32,19 +32,22 @@ window.onload = function () {
     ];
     element.uniforms = [
         c.createUniform("texture", "sampler2D"),
-        c.createUniform("projectionMatrix", "mat4")
+        c.createUniform("rotationMatrix", "mat4")
     ];
-    
+
     element.vertices = [
-        -0.5, -0.5, 0, 1,
-        0.5, -0.5, 1, 1,
-        -0.5, 0.5, 0, 0,
-        0.5, 0.5, 1, 0
+        -1, -1, 0, 1,
+        1, -1, 1, 1,
+        -1, 1, 0, 0,
+        1, 1, 1, 0
     ];
     element.indicies = [
         0, 1, 2,
         2, 1, 3
     ];
+
+    element.camera.projectionMatrix = c.MatrixMath.perspective(Math.PI/2, 1, 0.1, 1000);
+    element.camera.z = -2;
 
     var vertexShaderSrc = document.getElementById("vertexShaderElement").innerHTML;
     var fragmentShaderSrc = document.getElementById("fragmentShaderElement").innerHTML;
@@ -57,16 +60,17 @@ window.onload = function () {
 function loop() {
     c.clear();
 
-    var array = [];
-    for(var i=0;i<10;i++){
-        for(var j=0;j<10;j++){
-            var val = Math.sin((i + j + frame)/100);
-            array.push(val, val, val, val);
+    //
+    var data = [];
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            var val = parseInt((Math.sin((i + j + frame) / 10) + 1) / 2 * 255);
+            data.push(val, val, val, val);
         }
     }
-
-    c.setTexture(element.uniforms[0], 10, 10, array);
-    element.uniforms[1] = c.MatrixMath.yRotation(frame/300);
+    c.setTexture(element.uniforms[0], 10, 10, data);
+    
+    element.uniforms[1].value = c.MatrixMath.yRotation(frame / 100);
 
     c.renderFrame();
 
