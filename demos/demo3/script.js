816ed4e089ae3cb52c14111efa3804ecec78e812
htcryme <56440776+htcryme@users.noreply.github.com>
2020-08-16 14:29:28 +0200

    demo3 update

--- demos/demo3/script.js
+++ demos/demo3/script.js
@@ -2,7 +2,8 @@ var can
 var c;
 
 
-var element;
+var element1;
+var element2;
 
 var frame = 0;
 
@@ -26,38 +27,55 @@ window.onload = function () {
 
     c.clearColor(0.0, 0.0, 0.0, 0.0);
 
-    element = c.createElement();
-
-    element.attributes = [
+    element1 = c.createElement();
+    element1.attributes = [
         c.createAttribute("vertPosition", 2),
         c.createAttribute("texturePosition", 2),
     ];
-
-    element.uniforms = [
+    element1.uniforms = [
         c.createUniform("texture", "sampler2D")
     ];
+    c.setTextureToImage(element1.uniforms[0], "DogPicture.jpeg");
 
-    
-
-    c.setTextureToImage(element.uniforms[0], "DogPicture.jpeg");
-
-    
-
-    element.vertices = [
-        -1, -1, 0, 0,
-        1, -1, 1, 0,
-        -1, 1, 0, 1,
-        1, 1, 1, 1
+    element1.vertices = [
+        -1, -0.5, 0, 1,
+        0, -0.5, 1, 1,
+        -1, 0.5, 0, 0,
+        0, 0.5, 1, 0
     ];
-
-    element.indicies = [
+    element1.indicies = [
         0, 1, 2,
         2, 1, 3
     ];
 
-    var vertexShaderSrc = document.getElementById("vertexShader").innerHTML;
-    var fragmentShaderSrc = document.getElementById("fragmentShader").innerHTML;
-    c.addElement(element, vertexShaderSrc, fragmentShaderSrc);
+    var vertexShaderSrc = document.getElementById("vertexShaderElement1").innerHTML;
+    var fragmentShaderSrc = document.getElementById("fragmentShaderElement1").innerHTML;
+    c.addElement(element1, vertexShaderSrc, fragmentShaderSrc);
+
+
+    element2 = c.createElement();
+    element2.attributes = [
+        c.createAttribute("vertPosition", 2),
+        c.createAttribute("texturePosition", 2),
+    ];
+    element2.uniforms = [
+        element1.uniforms[0]
+    ];
+
+    element2.vertices = [
+        0, -0.5, 0, 1,
+        1, -0.5, 1, 1,
+        0, 0.5, 0, 0,
+        1, 0.5, 1, 0
+    ];
+    element2.indicies = [
+        0, 1, 2,
+        2, 1, 3
+    ];
+
+    var vertexShaderSrc = document.getElementById("vertexShaderElement2").innerHTML;
+    var fragmentShaderSrc = document.getElementById("fragmentShaderElement2").innerHTML;
+    c.addElement(element2, vertexShaderSrc, fragmentShaderSrc);
 
     frame = 0;
     loop();
@@ -65,8 +83,6 @@ window.onload = function () {
 
 function loop() {
     c.clear();
-
-
 
     c.renderFrame();
 
